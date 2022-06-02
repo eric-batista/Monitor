279bd26457cf91b5fb465b66dde5b668aac1e32e
Eric Batista <klose.eric31@gmail.com>
2022-06-02 14:33:17 -0300

    feat: create Redis cache manager for tweets-offset

--- nestjs-backend/src/tweets/check-new-tweets/check-new-tweets.task.ts
+++ nestjs-backend/src/tweets/check-new-tweets/check-new-tweets.task.ts
@@ -1,16 +1,41 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { TweetsService } from '../tweets.service';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class CheckNewTweetsTask {
   private readonly logger = new Logger(CheckNewTweetsTask.name);
   private limit = 10;
 
-  constructor(private tweetService: TweetsService) {}
+  constructor(
+    private tweetService: TweetsService,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+  ) {}
 
   @Interval(5000)
   async handle() {
     this.logger.log('Procurando tweets...');
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+
+    offset = offset === undefined || offset === null ? 0 : offset;
+    this.logger.log(`offset: ${offset}`);
+
+    const tweets = await this.tweetService.findAll({
+      offset,
+      limit: this.limit,
+    });
+
+    this.logger.log(`tweets count: ${tweets.length}`);
+
+    if (tweets.length === this.limit) {
+      this.logger.log('encontrou mais tweets');
+      await this.cacheManager.set('tweet-offset', offset + this.limit, {
+        ttl: 1 * 60 * 10,
+      });
+
+      this.logger.log('Disparo de e-mail');
+    }
   }
 }
--- nestjs-backend/src/tweets/tweets.module.ts
+++ nestjs-backend/src/tweets/tweets.module.ts
@@ -4,10 +4,15 @@ import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CheckNewTweetsTask } from './check-new-tweets/check-new-tweets.task';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
-    CacheModule.register({}),
+    CacheModule.register({
+      store: redisStore,
+      host: 'redis',
+      port: 6379,
+    }),
     MongooseModule.forFeature([{ name: Tweet.name, schema: TweetSchema }]),
   ],
   controllers: [TweetsController],
